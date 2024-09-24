54bbac9b7539b6f3e37711aca28f16e96633da68
Preyansh Jain <preyansh.jain2@cognizant.com>
2024-09-24 12:51:59 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,36 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Colors } from "./constants/styles";
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator
+        screenOptions={{
+          headerStyle: {backgroundColor:'yellow'}
+        }}
+        >
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Signup" component={SignUpScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ screens/LoginScreen.js
@@ -0,0 +1,13 @@
+import { Text, TextInput, View } from "react-native";
+
+function LoginScreen() {
+  return (
+    <View>
+      <Text>Email</Text>
+      <TextInput placeholder="enter email" />
+      {/* <Text>LoginScreen</Text> */}
+    </View>
+  );
+}
+
+export default LoginScreen;
